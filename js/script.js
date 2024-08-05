ea967b1bee1989fd1b2a00de10345439116d7eea
Mr-Don-Leo <maksimababayan@gmail.com>
2024-08-05 13:41:50 +0400

    sum

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 
 const islandBlock = document.querySelector('.island-block');
 const logo = document.getElementById('logo');
-const canvas = document.getElementById('codeCanvas');
-const ctx = canvas.getContext('2d');
 
 document.addEventListener('DOMContentLoaded', function() {
     logo.addEventListener('click', handleLogoClick);
@@ -12,60 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     initializeCanvas();
 });
-
-function initializeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    console.log(window.innerHeight);
-    console.log(window.innerWidth);
-
-    const lines = [];
-    const isMobile = window.innerWidth < 768;
-    const maxLines = isMobile ? 200 : 500;
-    const lineHeight = 20;
-
-    function getRandomCode() {
-        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[]{}.,;:<>?!@#$%^';
-        return chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-
-    function createLine() {
-        return {
-            x: Math.random() * canvas.width,
-            y: Math.random() * canvas.height,
-            text: getRandomCode(),
-            speed: Math.random() * (isMobile ? 1 : 3) + 1
-        };
-    }
-
-    for (let i = 0; i < maxLines; i++) {
-        lines.push(createLine());
-    }
-
-    function draw() {
-        ctx.fillStyle = '#262626';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-        ctx.fillStyle = '#0F0';
-        ctx.font = `${lineHeight}px monospace`;
-
-        lines.forEach(line => {
-            ctx.fillText(line.text, line.x, line.y);
-            line.y += line.speed;
-
-            if (line.y > canvas.height) {
-                line.y = 0;
-                line.x = Math.random() * canvas.width;
-                line.text = getRandomCode();
-            }
-        });
-
-        requestAnimationFrame(draw);
-    }
-
-    draw();
-}
 
 function handleLogoClick() {
     this.classList.remove('roll-in');
